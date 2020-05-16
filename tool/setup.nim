bbf028937d60ffe63c1a334ab177afa5c4461eba
jiro4989 <jiroron666@gmail.com>
2020-05-16 04:09:59 +0000

    add help

--- tool/setup.nim
+++ tool/setup.nim
@@ -1,4 +1,19 @@
-import strutils, os, strformat, times
+import strutils, os, strformat, times, parseopt
+
+const
+  doc = """
+setup setups this template repository.
+
+Usage:
+    setup [options]
+    setup (-h | --help)
+    setup --author:<author> --appname:<application_name>
+
+Options:
+    -h, --help                      Print this help
+    --author:<author>               Set author name
+    --appname:<application_name>    Set application name
+  """
 
 proc changeFile(beforeFile, afterFile, appName, author, dt: string) =
   let body =
@@ -13,9 +28,32 @@ proc changeFile(beforeFile, afterFile, appName, author, dt: string) =
   moveFile(beforeFile, afterFile)
   echo &"{beforeFile} was renamed {afterFile}."
 
+var
+  optParser = initOptParser(commandLineParams())
+  author = ""
+  appName = ""
+
+for kind, key, val in optParser.getopt():
+  case kind
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "help", "h":
+      echo doc
+      quit 0
+    of "author":
+      author = val
+    of "appname":
+      appName = val
+  of cmdEnd:
+    assert false # cannot happen
+  else: discard
+
+if author == "" or appName == "":
+  stderr.writeLine "'author' and 'appname' must be set."
+  stderr.writeLine "see 'setup -h'."
+  quit 1
+
 let
-  appName = commandLineParams()[0]
-  author = commandLineParams()[1]
   now = now().format("yyyy")
 
 changeFile("APPNAME.nimble", &"{appName}.nimble", appName, author, now)
@@ -24,3 +62,5 @@ changeFile("src"/"APPNAME.nim", "src" / &"{appName}.nim", appName, author, now)
 changeFile("tests"/"test1.nim", "", appName, author, now)
 changeFile("LICENSE", "", appName, author, now)
 changeFile(".github" / "workflows" / "main.yml", "", appName, author, now)
+
+echo "success."
